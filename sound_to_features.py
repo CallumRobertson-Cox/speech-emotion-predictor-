61c0f90c1a92ff0b637e13c22614e824db3795f3
CallumRobertson-Cox <callumsrephen@gmail.com>
2025-10-24 13:57:01 +0100

    Added ZCR and finalised get_features which returns a dictionary of all data

--- sound_to_features.py
+++ sound_to_features.py
@@ -1,22 +1,38 @@
 import librosa
 import numpy as np
 
-def main(audio):
-    pass
-
-def sound_to_volume(audio):
+def sound_to_volume(audio, sr):
     volume = librosa.feature.rms(y=audio)[0]
-    return np.mean(volume)
+    return float(np.mean(volume))
 
-def sound_to_pitch(audio):
-    pitch = librosa.yin(y=audio, fmin=50, fmax=300)
-    return np.mean(pitch)
+def sound_to_pitch(audio, sr):
+    pitch = librosa.yin(y=audio, fmin=50, fmax=300, sr=sr)
+    return float(np.mean(pitch))
     
-def sound_to_tempo(audio):
-    tempo, beats = librosa.beat.beat_track(y=audio)
+def sound_to_tempo(audio, sr):
+    tempo, beats = librosa.beat.beat_track(y=audio, sr =sr)
     beat_times = librosa.frames_to_time(beats)
     beat_intervals = np.diff(beat_times)
     return float(tempo[0]), float(np.mean(beat_intervals))
-    
+
+def sound_to_ZCR(audio, sr):
+    ZCR = librosa.feature.zero_crossing_rate(y=audio)[0]
+    return float(np.mean(ZCR))
+
+def get_features(audio, sr):
+    vol = sound_to_volume(audio, sr)
+    pitch = sound_to_pitch(audio, sr)
+    tempo, beats = sound_to_tempo(audio, sr)
+    ZCR = sound_to_ZCR(audio, sr)
+
+    return dict({
+        "volume" : vol,
+        "pitch" : pitch,
+        "tempo" : tempo,
+        "beats" : beats,
+        "ZCR" : ZCR
+    })
+
 #audio is the audio and sr is the sampling rate
-audio,sr= librosa.load("C:/Users/callu/OneDrive/Documents/Sounds/Background.mp3")
+audio,sr= librosa.load("C:/Users/callu/OneDrive/Documents/Sounds/Background.mp3", sr=10000)
+print(get_features(audio, sr))
