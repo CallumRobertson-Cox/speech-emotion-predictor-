a3cc20875d620ebd5fa092ee6d6139174c47acc5
CallumRobertson-Cox <callumsrephen@gmail.com>
2025-10-25 21:10:59 +0100

    All models peformed badly I need to change the features

--- sound_to_features.py
+++ sound_to_features.py
@@ -3,21 +3,21 @@ import numpy as np
 
 def sound_to_volume(audio, sr):
     volume = librosa.feature.rms(y=audio)[0]
-    return float(np.mean(volume))
+    return float(np.std(volume))
 
 def sound_to_pitch(audio, sr):
     pitch = librosa.yin(y=audio, fmin=50, fmax=300, sr=sr)
-    return float(np.mean(pitch))
+    return float(np.std(pitch))
     
 def sound_to_tempo(audio, sr):
     tempo, beats = librosa.beat.beat_track(y=audio, sr =sr)
     beat_times = librosa.frames_to_time(beats)
     beat_intervals = np.diff(beat_times)
-    return float(tempo[0]), float(np.mean(beat_intervals))
+    return float(tempo[0]), float(np.std(beat_intervals))
 
 def sound_to_ZCR(audio, sr):
     ZCR = librosa.feature.zero_crossing_rate(y=audio)[0]
-    return float(np.mean(ZCR))
+    return float(np.std(ZCR))
 
 def get_features(audio, sr):
     vol = sound_to_volume(audio, sr)
